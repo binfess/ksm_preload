72e75602fa1d9e99d1f3bf4775248955a1c03f85
binfess <fessdev@gmail.com>
2021-05-24 14:45:13 +0100

    Fix segfaults on hooks setup and add additional logs.

--- libksm_preload.c
+++ libksm_preload.c
@@ -42,22 +42,11 @@
 /* The default value for merge_threshold */
 static const char *const MERGE_THRESHOLD_ENV_NAME = "KSMP_MERGE_THRESHOLD";
 
-/* mmap2 is only defined on some 32 bit systems
- * (actually, it might only be defined i386)
- */
-#ifdef SYS_mmap2
-# define MMAP2_ENABLED 1
-#else
-# define MMAP2_ENABLED 0
-#endif
-
 #ifdef GCC
 # define likely(x)      __builtin_expect((x),1)
 #else
 # define likely(x)      (x)
 #endif
-
-
 
 /******** GLOBAL STATE ********/
 
@@ -68,8 +57,6 @@ typedef void *calloc_function (size_t nmemb, size_t size);
 typedef void *malloc_function (size_t size);
 typedef void *mmap_function (void *start, size_t length, int prot, int flags,
                              int fd, off_t offset);
-typedef void *mmap2_function (void *start, size_t length, int prot, int flags,
-                              int fd, off_t pgoffset);
 typedef void *mremap_function (void *old_address, size_t old_length,
                                size_t new_length, int flags, ...);
 typedef void *realloc_function (void *addr, size_t size);
@@ -77,45 +64,8 @@ typedef void *realloc_function (void *addr, size_t size);
 /* Declares the libc version of the functions we hook */
 extern calloc_function __libc_calloc;
 extern malloc_function __libc_malloc;
+extern mmap_function __mmap;
 extern realloc_function __libc_realloc;
-extern void __libc_free (void *ptr);
-
-/* Directly calls the mmap() syscall */
-static void *
-kernel_mmap (void *start, size_t length, int prot, int flags,
-             int fd, off_t offset)
-{
-#ifdef __i386__
-  uint32_t args[6] =
-    { (uint32_t) start, (uint32_t) length, prot, flags, fd, offset };
-  return (void *) syscall (SYS_mmap, args);
-#else
-  return (void *) syscall (SYS_mmap, start, length, prot, flags, fd, offset);
-#endif
-}
-
-#if MMAP2_ENABLED
-/* Directly calls the mmap2() syscall */
-static void *
-kernel_mmap2 (void *start, size_t length, int prot, int flags,
-              int fd, off_t pgoffset)
-{
-  assert (MMAP2_ENABLED);
-  return (void *) syscall (SYS_mmap2, start, length, prot, flags, fd,
-                           pgoffset);
-}
-#else
-/* If MMAP2_ENABLED is not defined, we won't export mmap2, so this
- * function should not be called. If it is called, it error()s.
- */
-static void *
-kernel_mmap2 ()
-{
-  error (1, 0, "ksm_preload: Fatal error: mmap2 was called but not"
-         " exported. Please contact unbrice@vleu.net .");
-  return NULL;
-}
-#endif
 
 /* This structure contains all global variables. */
 static struct
@@ -126,7 +76,6 @@ static struct
   calloc_function *ext_calloc;
   malloc_function *ext_malloc;
   mmap_function *ext_mmap;
-  mmap2_function *ext_mmap2;
   mremap_function *ext_mremap;
   realloc_function *ext_realloc;
   /* The page size, this value is temporary and will be fixed
@@ -140,60 +89,27 @@ static struct
 #if __GLIBC_PREREQ(2,11) || KSMP_FORCE_LIBC
   __libc_calloc,                // libc's calloc
   __libc_malloc,		// libc's malloc
-  kernel_mmap,			// calls kernel's mmap
-  kernel_mmap2,			// calls kernel's mmap2, fails if undefined
+  __mmap,			// libc's mmap
   NULL,				// mremap, unused during initialisation
-  __libc_realloc,		// libc realloc
+  __libc_realloc,		// libc's realloc
+  4096,				// page_size
+  4096 * 8			// merge threshold
 #else
 #error This version of ksm_preload has not been tested with your	\
   libC. Please define KSMP_FORCE_LIBC to 1 (-DKSMP_FORCE_LIBC=1) and	\
   tell me about the result.
 #endif
-  4096,				// page_size
-    4096 * 8			// merge threshold
 };
-
-
 
 /******** SETUP ********/
 
-/* puts()-like, only enabled in DEBUG mode  */
-static void
-debug_puts (const char *str)
-{
 #ifdef DEBUG
-  fprintf (stderr, "ksm_preload: %s\n", str);
+#define debug_printf(fmt, ...) fprintf(stderr, \
+				       "ksm_preload: " fmt "\n" __VA_OPT__(,) __VA_ARGS__)
+#define debug_puts(str) debug_printf(str)
 #else
-  (void) str;			// disables a warning about unused str
-#endif
-}
-
-/* printf()-like, only enabled in DEBUG mode  */
-static void
-debug_printf (const char *fmt, ...)
-{
-#ifdef DEBUG
-  char *strp;
-  va_list var_args;
-  va_start (var_args, fmt);
-  if (vasprintf (&strp, fmt, var_args) > 0)
-    debug_puts (strp);
-  else
-    debug_puts ("debug_printf failed");
-  va_end (var_args);
-#else
-  (void) fmt;			// disables a warning about unused fmt
-#endif
-}
-
-#ifdef GCC
-#if __GNUC_PREREQ(4,0)
-// marks debug_printf as a printf-like function in order to get warnings
-// if it is used incorectly
-static void
-debug_printf (const char *fmt, ...)
-__attribute__ ((format (gnu_printf, 1, 2)));
-#endif
+#define debug_printf(fmt, ...)
+#define debug_puts(str)
 #endif
 
 /* Gets an environment variable from its name and parses it as a
@@ -257,17 +173,6 @@ setup ()
   mremap_function *dl_mremap = xdlsym (RTLD_NEXT, "mremap");
   realloc_function *dl_realloc = xdlsym (RTLD_NEXT, "realloc");
 
-  /* Treats mmap2 as a special case because dlsym fails to find it on
-   * Ubuntu 10.10, even when it is available.
-   */
-  // TODO(unbrice): investigate...
-  if (MMAP2_ENABLED)
-    {
-      mmap2_function *dl_mmap2 = dlsym (RTLD_NEXT, "mmap2");
-      if (NULL != dl_mmap2)
-	globals.ext_mmap2 = dl_mmap2;
-    }
-
   /* Get parameters from the environment */
   globals.page_size = (long unsigned) sysconf (_SC_PAGESIZE);
   env_merge_treshold = get_int_from_environment (MERGE_THRESHOLD_ENV_NAME);
@@ -283,7 +188,6 @@ setup ()
 
   debug_puts ("Setup done.");
 }
-
 
 /******** UTILITIES FOR WRAPPERS ********/
 
@@ -341,12 +245,11 @@ merge_if_profitable (void *address, size_t length, int flags)
       if (0 != madvise ((void *) page_address, new_length, MADV_MERGEABLE))
 	debug_puts ("madvise() failed");
       else
-	debug_puts ("Sharing");
+	debug_printf ("Sharing %zu bytes from %p", new_length, page_address);
     }
   else
     debug_puts ("Not sharing (flags filtered)");
 }
-
 
 /******** WRAPPERS ********/
 
@@ -356,6 +259,7 @@ calloc (size_t nmemb, size_t size)
 {
   lazily_setup ();
   void *res = globals.ext_calloc (nmemb, size);
+  debug_printf ("calloc (%zu, %zu) = %p", nmemb, size, res);
   merge_if_profitable (res, size, -1);
   return res;
 }
@@ -366,6 +270,7 @@ malloc (size_t size)
 {
   lazily_setup ();
   void *res = globals.ext_malloc (size);
+  debug_printf ("malloc (%zu) = %p", size, res);
   merge_if_profitable (res, size, -1);
   return res;
 }
@@ -376,23 +281,8 @@ mmap (void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
   lazily_setup ();
   void *res = globals.ext_mmap (addr, length, prot, flags, fd, offset);
-  merge_if_profitable (res, length, flags);
-  return res;
-}
-
-#if ! MMAP2_ENABLED
-/* Hides mmap2 */
-static void *mmap2 (void *addr, size_t length, int prot, int flags, int fd,
-		    off_t pgoffset) __attribute__ ((unused));
-#endif
-
-/* Just like mmap2() but calls merge_if_profitable */
-void *
-mmap2 (void *addr, size_t length, int prot, int flags, int fd, off_t pgoffset)
-{
-  assert (MMAP2_ENABLED);
-  lazily_setup ();
-  void *res = globals.ext_mmap2 (addr, length, prot, flags, fd, pgoffset);
+  debug_printf ("mmap (%p, %zu, %d, %d, %d, %llu) = %p",
+		addr, length, prot, flags, fd, (unsigned long long)offset, res);
   merge_if_profitable (res, length, flags);
   return res;
 }
@@ -418,6 +308,8 @@ mremap (void *old_address, size_t old_length, size_t new_length, int flags,
     }
   else
     res = globals.ext_mremap (old_address, old_length, new_length, flags);
+  debug_printf ("mremap (%p, %zu, %zu, %d, ...) = %p",
+		old_address, old_length, new_length, flags, res);
   merge_if_profitable (res, new_length, -1);
   return res;
 }
@@ -428,6 +320,7 @@ realloc (void *addr, size_t size)
 {
   lazily_setup ();
   void *res = globals.ext_realloc (addr, size);
+  debug_printf ("realloc (%p, %zu) = %p", addr, size, res);
   merge_if_profitable (res, size, -1);
   return res;
 }
